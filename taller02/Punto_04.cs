5fc6bff74cefe62d3e4b7965bd679cbbc715a6d3
JoseaSosa <josea_1998@htomail.com>
2023-03-16 11:51:01 -0500

    se soluciona taller de C#

---
+++ taller02/Punto_04.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.CodeDom;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.ExceptionServices;
+using System.Runtime.Serialization.Formatters;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace taller02
+{
+    internal class Punto_04
+    {
+        static void Main(string[] args) {
+
+            int final = numeroMayor();
+
+            Console.WriteLine("El numero mayor es: " + final);
+           
+        }
+        static int numeroMayor()
+        {
+            int mayor = 0;
+
+            for (int i = 0; i < 4; i++)
+            {
+                Console.Write("Ingrese numero " + (i + 1));
+                int num = int.Parse(Console.ReadLine());
+                if (num > mayor) {
+                    mayor = num;
+                }
+            }
+            return mayor;
+        }
+    }
+}
